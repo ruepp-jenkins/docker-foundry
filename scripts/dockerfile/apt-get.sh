9c98791dc6f5f6562656083ce33f23120771955c
MyUncleSam <12820919+MyUncleSam@users.noreply.github.com>
2024-05-06 21:49:45 +0200

    -

--- scripts/dockerfile/apt-get.sh
+++ scripts/dockerfile/apt-get.sh
@@ -18,5 +18,7 @@ add-apt-repository -y -n multiverse
 dpkg --add-architecture i386
 
 echo "Install additional packages"
+echo steam steam/question select "I AGREE" | debconf-set-selections
+echo steam steam/license note '' | debconf-set-selections
 apt-get update
 apt-get install -y steamcmd
