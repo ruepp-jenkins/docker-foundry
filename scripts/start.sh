037f3c4530e1aa6222a4aa54564a4b27c7e9e1d8
MyUncleSam <12820919+MyUncleSam@users.noreply.github.com>
2024-05-07 23:13:09 +0200

    ---

--- scripts/start.sh
+++ scripts/start.sh
@@ -8,9 +8,7 @@ scripts/docker_initialize.sh
 echo "Building image:  ${IMAGE_FULLNAME}"
 docker build \
     -t ${IMAGE_FULLNAME}:latest \
-    .
-
-docker push ${IMAGE_FULLNAME}:latest
+    --push .
 
 # cleanup
 scripts/docker_cleanup.sh
